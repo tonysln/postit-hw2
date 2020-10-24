b62aeb03f8cf00ba9328e59024f0f5064587bb58
SanRoosalu <roosalu.sander@gmail.com>
2020-10-24 19:13:30 +0300

    browse.html: Added base profiles to main-container; style.css: added styling to profiles; script.js: added semi-jQuery toggle for follow button.

--- res/js/script.js
+++ res/js/script.js
@@ -12,6 +12,13 @@ $(function() {
     $("#user-avatar").click(function() {
         $("#drop-down-menu").toggle()
     })
+
+    //Toggle follow button
+    $('button.follow-button').click(function () {
+        $(this).toggleClass('followed-button')
+        if (this.innerHTML === "Follow") this.innerHTML = "Followed"
+        else this.innerHTML = "Follow"
+    });
 })
 
 function displayUserInfo(user) {
